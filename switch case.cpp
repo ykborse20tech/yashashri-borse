7d79e1f4b6a13c5e67775a9cbee7353c0c0891b0
ykborse20tech <ykborse2015@gmail.com>
2026-03-28 10:54:08 +0530

    Add files via upload

---
+++ switch case.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+	int choice;
+	double num1, num2, result;
+	do {
+		
+		printf("\n===== Arithmetic Calculator =====\n");
+		printf("1. Addition \n");
+		printf("2. Subtraction \n");
+		printf("3. Multiplication \n");
+		printf("4. Division \n");
+		printf("5. Exit\n");
+		printf("Enter your choice (1-5):");
+		scanf("%d",&choice);
+		
+		switch (choice)	{
+			 case 1:
+				printf("Enter two numbers: ";)
+				scanf("%lf %lf", &num1, &num2);
+				result = num1 +num2;
+				printf("Result: %21f = %21f = %21f\n" , num1, num2, result);
+				break;
+			 case 2:
+				printf("Enter two numbers: ";)
+				scanf("%lf %lf", &num1, &num2);
+				result = num1 - num2;
+				printf("Result: %21f = %21f = %21f\n" , num1, num2, result);
+				break;
+			case 3:
+				printf("Enter two numbers: ";)
+				scanf("%lf %lf", &num1, &num2);
+				result = num1 * num2;
+				printf("Result: %21f = %21f = %21f\n" , num1, num2, result);
+				break;
+			case 4:
+				printf("Enter two numbers: ");
+				scanf
+				
+		}
+}

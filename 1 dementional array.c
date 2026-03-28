670f19fe98071a6277e827e38ad29809c3ee7de6
ykborse20tech <ykborse2015@gmail.com>
2026-03-28 10:39:52 +0530

    Add files via upload

---
+++ 1 dementional array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int array[8]={10,20,30,40,50,60,70,80};
+	int i;
+	printf("Element of the array are:\n ");
+	for(i=0;i<8;i++);
+	{
+	printf("array[%d]=%d\n",i,array[i]);
+}
+return 0;
+}
